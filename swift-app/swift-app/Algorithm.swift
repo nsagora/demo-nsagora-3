aaf8342529cabe5db5685b8652d17dc3f5e972ca
Alex Cristea <alexcristea@me.com>
2015-01-10 10:42:04 +0200

    Added asyn calculation for sum and asyn tests for sum

--- swift-app/swift-app/Algorithm.swift
+++ swift-app/swift-app/Algorithm.swift
@@ -19,7 +19,28 @@ class Algorithm {
         return sum;
     }
     
+    class func sum(n:Double, callback:(sum:Double)->Void) {
+    
+        let queue = dispatch_queue_create("queue.summ.algorithm", nil);
+        dispatch_async(queue) {
+            let sum = self.summ(n)
+            dispatch_async(dispatch_get_main_queue()) {
+                callback(sum:sum);
+            }
+        }
+    }
+    
     class func fibonacci(n:Int)->Int {
         return (n < 2) ? 1 : self.fibonacci(n-1) + self.fibonacci(n-2);
+    }
+    
+    class func fibonacci(n:Int, callback:(fibonacci:Int)->Void) {
+        let queue = dispatch_queue_create("fibonacci.summ.algorithm", nil);
+        dispatch_async(queue, {
+            let fibonacci = self.fibonacci(n);
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                callback(fibonacci: fibonacci);
+            })
+        });
     }
 }
--- swift-app/swift-appTests/SumTests.swift
+++ swift-app/swift-appTests/SumTests.swift
@@ -9,11 +9,76 @@
 import UIKit
 import XCTest
 
+// MARK: Unit test
+
 class SumTests: XCTestCase {
    
+    func testSumOfZero () {
+        let sum = Algorithm.summ(0);
+        
+        XCTAssertEqual(sum, 0, "Sum must be 0")
+    }
+    
+    func testSumOfOne () {
+        let sum = Algorithm.summ(1);
+        XCTAssertEqual(sum, 1, "Sum must be 1")
+    }
+    
+    func testSumOfTwo () {
+        let sum = Algorithm.summ(2);
+        XCTAssertEqual(sum, 3, "Sum must be 3")
+    }
+    
+}
+
+// MARK: Async test
+
+extension SumTests {
+    
+    func testSumOf_1_000_000_Async () {
+        
+        let expectedSum:Double = 500_000_500_000;
+        let n:Double = 1_000_000;
+        var result:Double = 0;
+        
+        let expectation = expectationWithDescription("Expectation for asyn sum calculator");
+        
+        Algorithm.sum(n) { sum in
+            result = sum;
+            expectation.fulfill();
+        }
+        
+        waitForExpectationsWithTimeout(1, handler: nil);
+        
+        XCTAssertEqual(result, expectedSum, "The sum of an \(n) must me equal to \(expectedSum)");
+    }
+    
+    func testSumOf_5_000_000_Async () {
+        
+        let expectedSum:Double = 12_500_002_500_000;
+        let n:Double = 5_000_000;
+        var result:Double = 0;
+        
+        let expectation = expectationWithDescription("Expectation for asyn sum calculator");
+        
+        Algorithm.sum(n) { sum in
+            result = sum;
+            expectation.fulfill();
+        }
+        
+        waitForExpectationsWithTimeout(1, handler: nil);
+        
+        XCTAssertEqual(result, expectedSum, "The sum of an \(n) must me equal to \(expectedSum)");
+    }
+}
+
+// MARK: Perfromance test
+
+extension SumTests {
+
     func testMesureBlock() {
         
-        measureMetrics([XCTPerformanceMetric_WallClockTime], automaticallyStartMeasuring: false) { () -> Void in
+        measureMetrics([XCTPerformanceMetric_WallClockTime], automaticallyStartMeasuring: false) {
             var x = Algorithm.summ(1_000_000);
             
             self.startMeasuring();
